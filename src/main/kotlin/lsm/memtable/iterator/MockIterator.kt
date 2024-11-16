6aeb9168aab8bb8ad6ab9d8d50f0850f4af02fa3
김선우 <seonwoo960000@toss.im>
2024-11-16 21:08:40 +0900

    Add MockIterator

---
+++ src/main/kotlin/lsm/memtable/iterator/MockIterator.kt
@@ -0,0 +1,26 @@
+package org.example.lsm.memtable.iterator
+
+import org.example.common.ComparableByteArray
+
+class MockIterator(
+    private val data: List<Pair<ComparableByteArray, ComparableByteArray>>
+) : StorageIterator {
+
+    private var currentIdx: Int = 0
+
+    override fun key(): ComparableByteArray {
+        return data[currentIdx].first
+    }
+
+    override fun value(): ComparableByteArray {
+        return data[currentIdx].second
+    }
+
+    override fun isValid(): Boolean {
+        return currentIdx < data.size
+    }
+
+    override fun next() {
+        currentIdx++
+    }
+}
