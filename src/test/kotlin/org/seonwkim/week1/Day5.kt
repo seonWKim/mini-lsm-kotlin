6272d4c28ab9bb9541dbf850bc393ceab54cd659
김선우 <seonwoo960000@toss.im>
2024-11-19 21:17:44 +0900

    Add Week1 Day5 test (2)

--- src/test/kotlin/org/seonwkim/week1/Day5.kt
+++ src/test/kotlin/org/seonwkim/week1/Day5.kt
@@ -37,6 +37,32 @@ class Day5 {
         ))
     }
 
+    @Test
+    fun `test task2 merge 2`() {
+        val i2 = MockIterator(
+            listOf(
+                MockIteratorData("a".toComparableByteArray(), "1.1".toComparableByteArray()),
+                MockIteratorData("b".toComparableByteArray(), "2.1".toComparableByteArray()),
+                MockIteratorData("c".toComparableByteArray(), "3.1".toComparableByteArray()),
+            )
+        )
+        val i1 = MockIterator(
+            listOf(
+                MockIteratorData("a".toComparableByteArray(), "1.2".toComparableByteArray()),
+                MockIteratorData("b".toComparableByteArray(), "2.2".toComparableByteArray()),
+                MockIteratorData("c".toComparableByteArray(), "3.2".toComparableByteArray()),
+                MockIteratorData("d".toComparableByteArray(), "4.2".toComparableByteArray()),
+            )
+        )
+        val iter = TwoMergeIterator.create(i1, i2)
+        checkIterator(iter, listOf(
+            Pair("a".toComparableByteArray(), "1.2".toComparableByteArray()),
+            Pair("b".toComparableByteArray(), "2.2".toComparableByteArray()),
+            Pair("c".toComparableByteArray(), "3.2".toComparableByteArray()),
+            Pair("d".toComparableByteArray(), "4.2".toComparableByteArray()),
+        ))
+    }
+
     private fun checkIterator(
         actual: StorageIterator,
         expected: List<Pair<ComparableByteArray, ComparableByteArray>>
