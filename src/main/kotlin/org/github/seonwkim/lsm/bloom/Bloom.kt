6c7dd5b0f0849da061a2fa3bd615e83229c23d05
김선우 <seonwoo960000@toss.im>
2024-11-22 17:08:31 +0900

    Add rotateLeft function

--- src/main/kotlin/org/github/seonwkim/lsm/bloom/Bloom.kt
+++ src/main/kotlin/org/github/seonwkim/lsm/bloom/Bloom.kt
@@ -1,32 +1,37 @@
-//package org.github.seonwkim.lsm.bloom
-//
-//import org.github.seonwkim.common.clamp
-//import org.github.seonwkim.common.rotateLeft
-//
-///**
-// * Bloom filter implementation.
-// */
-//class Bloom private constructor(
-//    // data of filter in bits
-//    val filter: List<Byte>,
-//
-//    // number of hash functions
-//    val k: Long
-//) {
-//
-//    companion object {
-//        fun fromKeyHashes(keys: List<Int>, bitsPerKey: Int): Bloom {
-//            val k = (bitsPerKey * 0.69).toInt().clamp(1, 30)
-//            var nbits = maxOf(keys.size * bitsPerKey, 64)
-//
-//            val nbytes = (nbits + 7) / 8
-//            nbits = nbytes * 8
-//
-//            val filter = ByteArray(nbytes) { 0 }
-//            for (h in keys) {
-//                val delta = h.rotateLeft(15)
-//
-//            }
-//        }
-//    }
-//}
+package org.github.seonwkim.lsm.bloom
+
+import org.github.seonwkim.common.clamp
+import org.github.seonwkim.common.rotateLeft
+
+/**
+ * Bloom filter implementation.
+ */
+class Bloom private constructor(
+    // data of filter in bits
+    val filter: List<Byte>,
+
+    // number of hash functions
+    val k: Int
+) {
+
+    companion object {
+        fun fromKeyHashes(keys: List<Int>, bitsPerKey: Int): Bloom {
+            val k = (bitsPerKey * 0.69).toInt().clamp(1, 30)
+            var nbits = maxOf(keys.size * bitsPerKey, 64)
+            val nbytes = (nbits + 7) / 8
+            nbits = nbytes * 8
+
+            val filter = ByteArray(nbytes) { 0 }
+            for (key in keys) {
+                var h = key
+                val delta = h.rotateLeft(15)
+                repeat(k) {
+                    val bitPosition = (h % nbits).toInt()
+                    filter[bitPosition] = 1
+                    h += delta
+                }
+            }
+            return Bloom(filter.toList(), k)
+        }
+    }
+}
--- src/test/kotlin/org/github/seonwkim/common/ExtensionsTest.kt
+++ src/test/kotlin/org/github/seonwkim/common/ExtensionsTest.kt
@@ -6,16 +6,16 @@ import kotlin.test.Test
 class ExtensionsTest {
     @Test
     fun `test rotateLeft`() {
-        val int1 = 0b00000000000000000000000000000001u
+        val int1 = 0b00000000000000000000000000000001
         val actual1 = int1.rotateLeft(3)
-        assertEquals(0b00000000000000000000000000001000u, actual1)
+        assertEquals(0b00000000000000000000000000001000, actual1)
 
-        val int2 = 0b11000000000000000000000000000000u
+        val int2 = 0b01100000000000000000000000000000
         val actual2 = int2.rotateLeft(3)
-        assertEquals(0b00000000000000000000000000000110u, actual2)
+        assertEquals(0b00000000000000000000000000000011, actual2)
 
-        val int3 = 0b00000000000000000000000000000111u
+        val int3 = 0b00000000000000000000000000000111
         val actual3 = int3.rotateLeft(0)
-        assertEquals(0b00000000000000000000000000000111u, actual3)
+        assertEquals(0b00000000000000000000000000000111, actual3)
     }
 }
