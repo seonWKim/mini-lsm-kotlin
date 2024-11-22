73075d046814cb9faf3890bb24635087a80c26a6
김선우 <seonwoo960000@toss.im>
2024-11-22 13:30:51 +0900

    Nit

--- src/main/kotlin/org/github/seonwkim/common/Definitions.kt
+++ src/main/kotlin/org/github/seonwkim/common/Definitions.kt
@@ -2,8 +2,8 @@ package org.github.seonwkim.common
 
 // Size of a `Short` which is 2 bytes in JVM
 // TODO: Are there any situations where SIZEOF_U16 is not 2?
-const val SIZE_OF_U16_IN_BYTE: Int = 2
-const val SIZE_OF_U32_IN_BYTE: Int = 4
-const val SIZE_OF_U64_IN_BYTE: Int = 8
+const val SIZE_OF_U16_IN_BYTE: Int = Short.SIZE_BYTES
+const val SIZE_OF_U32_IN_BYTE: Int = Int.SIZE_BYTES
+const val SIZE_OF_U64_IN_BYTE: Int = Long.SIZE_BYTES
 
 const val TS_DEFAULT: Long = 0L
