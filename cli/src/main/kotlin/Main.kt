e6c4846ad21bb9c61e99ea37e8ee01321d8a3d19
김선우 <seonwoo960000@toss.im>
2024-11-24 12:10:04 +0900

    Nit

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -2,12 +2,14 @@ package org.github.seonwkim
 
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
+import kotlinx.cli.ExperimentalCli
 import kotlinx.cli.Subcommand
 import org.github.seonwkim.lsm.storage.LsmStorageOptions
 import org.github.seonwkim.lsm.storage.MiniLsm
 import org.github.seonwkim.lsm.storage.NoCompaction
 import java.nio.file.Paths
 
+@OptIn(ExperimentalCli::class)
 fun main() {
     val path = Paths.get("").toAbsolutePath().resolve("cli-output")
     val miniLsm = MiniLsm.open(
