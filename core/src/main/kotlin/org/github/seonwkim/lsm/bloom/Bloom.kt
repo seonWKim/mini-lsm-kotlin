b73fb14d8338e1210e61c11f58816edba3634f7e
김선우 <seonwoo960000@toss.im>
2024-12-01 10:28:35 +0900

    Add docs for bloom package

--- core/src/main/kotlin/org/github/seonwkim/lsm/bloom/Bloom.kt
+++ core/src/main/kotlin/org/github/seonwkim/lsm/bloom/Bloom.kt
@@ -7,21 +7,35 @@ import org.github.seonwkim.common.setBit
 
 /**
  * Bloom filter implementation.
+ *
+ * @property filter the data of the filter in bits
+ * @property hashFunctionsCount the number of hash functions
+ * @property nbits the number of bits in the filter (8 * filter.size)
  */
-class Bloom(
-    // data of filter in bits
+class Bloom private constructor(
     val filter: ComparableByteArray,
-
-    // number of hash functions
     val hashFunctionsCount: Int,
-
-    // Number of bites in filters which is 8 * filter.size
     private val nbits: UInt
 ) {
 
     companion object {
         private const val ROTATE_BIT_COUNT = 15
 
+        fun create(filter: ComparableByteArray, hashFunctionsCount: Int): Bloom {
+            return Bloom(
+                filter = filter,
+                hashFunctionsCount = hashFunctionsCount,
+                nbits = filter.size().toUInt() * 8u
+            )
+        }
+
+        /**
+         * Creates a Bloom filter from a list of key hashes.
+         *
+         * @param keys the list of key hashes
+         * @param bitsPerKey the number of bits per key
+         * @return a new Bloom filter
+         */
         fun fromKeyHashes(keys: List<UInt>, bitsPerKey: Int): Bloom {
             val hashFunctionsCount = (bitsPerKey * 0.69).toInt().clamp(1, 30)
             var nbits = maxOf(keys.size.toUInt() * bitsPerKey.toUInt(), 64u)
@@ -47,6 +61,12 @@ class Bloom(
         }
     }
 
+    /**
+     * Checks if the Bloom filter may contain the given key.
+     *
+     * @param key the key to check
+     * @return true if the Bloom filter may contain the key, false otherwise
+     */
     fun mayContain(key: UInt): Boolean {
         var hashKey = key
         val delta = hashKey.rotateLeft(ROTATE_BIT_COUNT)
--- core/src/main/kotlin/org/github/seonwkim/lsm/bloom/BloomUtil.kt
+++ core/src/main/kotlin/org/github/seonwkim/lsm/bloom/BloomUtil.kt
@@ -5,11 +5,18 @@ import kotlin.math.ceil
 import kotlin.math.ln
 import kotlin.math.pow
 
+/**
+ * Utility object for encoding, decoding, and calculating parameters for Bloom filters.
+ */
 object BloomUtil {
     private val LN_2_POW_2 = ln(2.0).pow(2)
 
     /**
-     * Get bloom filter bits per key from [numberOfEntries] and [falsePositiveRate].
+     * Calculates the number of bits per key for a Bloom filter based on the number of entries and the desired false positive rate.
+     *
+     * @param numberOfEntries the number of entries expected in the Bloom filter
+     * @param falsePositiveRate the desired false positive rate
+     * @return the number of bits per key
      */
     fun bloomBitsPerKey(numberOfEntries: Int, falsePositiveRate: Double): Int {
         val entriesDouble = numberOfEntries.toDouble()
@@ -18,6 +25,12 @@ object BloomUtil {
         return locs.toInt()
     }
 
+    /**
+     * Encodes a Bloom filter into a buffer.
+     *
+     * @param buf the buffer to encode the Bloom filter into
+     * @param bloom the Bloom filter to encode
+     */
     fun encode(buf: ComparableByteArray, bloom: Bloom) {
         val offset = buf.size()
         buf.append(bloom.filter)
@@ -26,6 +39,13 @@ object BloomUtil {
         buf.append(checksum.toU32ByteArray())
     }
 
+    /**
+     * Decodes a Bloom filter from a buffer.
+     *
+     * @param buf the buffer containing the encoded Bloom filter
+     * @return the decoded Bloom filter
+     * @throws Error if the checksum does not match
+     */
     fun decode(buf: ComparableByteArray): Bloom {
         val bufSize = buf.size()
         val checksum = buf.slice(bufSize - 4..<bufSize).toU32Int()
@@ -35,10 +55,9 @@ object BloomUtil {
 
         val hashFunctionsCount = buf[bufSize - 5].toInt()
         val filter = buf.slice(0, bufSize - 5)
-        return Bloom(
+        return Bloom.create(
             filter = filter,
             hashFunctionsCount = hashFunctionsCount,
-            nbits = filter.size().toUInt() * 8u
         )
     }
 }
