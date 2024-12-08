f9eae995b17efcba8549c42f6dca2882278ae7b8
김선우 <seonwoo960000@toss.im>
2024-12-09 00:15:17 +0900

    Change TS_ENABLED to true

--- mini-lsm-mvcc/src/main/kotlin/org/github/seonwkim/lsm/Configuration.kt
+++ mini-lsm-mvcc/src/main/kotlin/org/github/seonwkim/lsm/Configuration.kt
@@ -1,5 +1,5 @@
 package org.github.seonwkim.lsm
 
 object Configuration {
-    val TS_ENABLED: Boolean = System.getenv("MINI_LSM_TS_ENABLED")?.toBoolean() ?: false
+    val TS_ENABLED: Boolean = System.getenv("MINI_LSM_TS_ENABLED")?.toBoolean() ?: true
 }
